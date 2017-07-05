9fc8af848448dd1df298dd51f0a72a606bf269e6
Daniel Cardona <decardona34@misena.edu.co>
2017-07-04 19:36:40 -0500

    Header y Twitter responsive en WebSite
    Completado la parte de diseño tanto del header como del Website

---
+++ website/index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+      <title>Julian Osorio Music</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700|Roboto+Condensed:400,700|Roboto:300,400,700" >
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="views/assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="views/assets/css/index.css">
+  </head>
+  <body>
+      <main class="main-container">
+        <header class="header-container">
+          <div class="header--navbar">
+            <div class="header--logo">
+                <img src="views/assets/img/Recursos/logo/Logo.png" alt="" class="img--logo">
+            </div>
+            <i class="header--icon">
+              <img class="image--icon" src="views/assets/img/Recursos/icons/menu.svg" alt="">
+            </i>
+            <div class="navbar-container">
+                <nav class="navbar--contain">
+                    <ul class="navbar--items">
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Inicio</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Infinito</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Sobre mí</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Blog</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Eventos</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Galeria</a>
+                        </li>
+                        <li class="navbar--item">
+                            <a href="#" class="navbar--link">Contáctame</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+          </div>
+          <div class="header--text">
+            <div class="header--text__container">
+              <div class="header--text__contain">
+                <h6 class="header--text__subtitle">Descarga y lleva siempre contigo</h6>
+                <h1 class="header--text__title">Infinito</h1>
+                <a href="#" class="header--button">Disponible en<img class="button--image" src="views/assets/img/Recursos/icons/google-play.png" alt="">
+              </a>
+              </div>
+            </div>
+          </div>
+          <section class="player-container">
+            <h2 style="font-size: 16px;">Canción para mentir</h2>
+          </section>
+        </header>
+        <section class="twitter-container">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <div class="twitter--body">
+                  <i class="fa fa-twitter fa-2x twitter--icon"></i>
+                  <h2 class="twitter--name">@julianosoriomusic</h2>
+                  <p class="twitter--text">Nunc ligula augue, aliquam et nulla non, bibendum imperdiet di</p>
+                  <h4 class="twitter--date">Publicado 12 de Junio</h4>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="twitter--body">
+                  <i class="fa fa-twitter fa-2x twitter--icon"></i>
+                  <h2 class="twitter--name">@julianosoriomusic</h2>
+                  <p class="twitter--text">Nunc ligula augue, aliquam et nulla non, bibendum imperdiet di</p>
+                  <h4 class="twitter--date">Publicado 12 de Agosto</h4>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="twitter--body">
+                  <i class="fa fa-twitter fa-2x twitter--icon"></i>
+                  <h2 class="twitter--name">@julianosoriomusic</h2>
+                  <p class="twitter--text">Nunc ligula augue, aliquam et nulla non, bibendum imperdiet di</p>
+                  <h4 class="twitter--date">Publicado 12 de Septiembre</h4>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="twitter--body">
+                  <i class="fa fa-twitter fa-2x twitter--icon"></i>
+                  <h2 class="twitter--name">@julianosoriomusic</h2>
+                  <p class="twitter--text">Nunc ligula augue, aliquam et nulla non, bibendum imperdiet di, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus inventore nihil quisquam tenetur iure aperiam consequatur.</p>
+                  <h4 class="twitter--date">Publicado 12 de Noviembre</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>  
+        <section class="infinito-container">
+          <div class="infinito--header">
+            <img src="views/assets/img/Recursos/header/header-Music.png" alt="">
+            <h4 class="infinito--subtitle">Elit neque ullamcorper eros</h4>
+            <h2 class="infinito--title">Infinito</h2>
+          </div>
+          <div class="infinito--player">
+             <div class="player--songs">
+               
+             </div>
+             <div class="player--album">
+               
+             </div>
+          </div>
+        </section>
+        <section class="about-me--container">
+          <div class="about-me--header">
+            <img src="views/assets/img/Recursos/header/header-about.png" alt="">
+            <h2 class="about-me--title">Sobre Mí</h2>
+          </div>
+          <div class="about-me-body">
+             <div class="about-me-content">
+              <div class="about-me--image">
+                <img src="views/assets/img/Recursos/images/julian-infinito.png" alt="">
+              </div>
+              <div class="about-me--principal">
+                <h4 class="about-me--text">
+                  “Phasellus varius ullamcorper magna id sagittis. In luctus lorem turpis, ac sollicitudin eros auctor nec. Ut laoreet finibus ante non iaculis. Ut lorem tortor”
+                </h4>
+                <h6 class="about-me--name">
+                  - Julian Osorio
+                </h6>
+              </div>
+              <div class="about-me--section">
+                <p class="about-me--pharagraf">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor purus quis bibendum elementum. Nulla luctus, velit vel dignissim volutpat, elit neque ullamcorper eros, vel commodo enim ex vel erat. Etiam nisi odio, dictum in scelerisque ut, accumsan in tortor. Etiam consequat nibh sit amet varius posuere. Integer pulvinar justo sed ante egestas, sit amet molestie velit lacinia. 
+
+                  Donec at nulla quis massa egestas tempor at eget est. Maecenas eget laoreet ante, ut egestas turpis. Maecenas sed elit vel arcu sagittis suscipit. Aliquam erat volutpat. Sed sit amet arcu et elit congue bibendum eget in ante. Nunc ligula augue, aliquam et nulla non, bibendum imperdiet diam. Phasellus semper, nisi at fringilla rhoncus, mauris diam dapibus purus, eu varius odio mi vitae mauris. Aenean pellentesque diam luctus augue dapibus luctus.
+                </p>
+              </div>
+             </div>
+          </div>
+        </section>
+        <section class="blog--container">
+          <div class="blog--header">
+            <img src="views/assets/img/Recursos/header/header-blog.png" alt="">
+            <h2 class="blog--title">Blog</h2>
+          </div>
+          <div class="blog--body">
+            <article class="blog--card">
+              <div class="blog--card__header">
+                <img src="views/assets/img/Recursos/images/img-blog-1.png" alt="">
+                <div class="blog--date">
+                  <h4 class="blog--date__title">
+                    13
+                  </h4>
+                  <h6 class="blog--date__subtitle">
+                    Junio
+                  </h6>
+                </div>
+                <div class="blog--card__title">
+                  <h6>130 Lecturas</h6>
+                  <h6>2 Comentarios</h6>
+                </div>
+              </div>
+              <div class="blog--card__body">
+                <div class="blog--card__pharagraf">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim.
+                </div>
+              </div>
+              <div class="blog--card__footer">
+                <div class="blog--button">
+                  <a href="#">
+                    Leer Más
+                    <i class="fa fa-chevron-right"></i>
+                  </a>
+                </div>
+                <div class="blog--social">
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-google-plus"></i></a>
+                </div>
+              </div>
+            </article>
+            <article class="blog--card">
+              <div class="blog--card__header">
+                <img src="views/assets/img/Recursos/images/img-blog-2.png" alt="">
+                <div class="blog--date">
+                  <h4 class="blog--date__title">
+                    13
+                  </h4>
+                  <h6 class="blog--date__subtitle">
+                    Junio
+                  </h6>
+                </div>
+                <div class="blog--card__title">
+                  <h6>130 Lecturas</h6>
+                  <h6>2 Comentarios</h6>
+                </div>
+              </div>
+              <div class="blog--card__body">
+                <div class="blog--card__pharagraf">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim.
+                </div>
+              </div>
+              <div class="blog--card__footer">
+                <div class="blog--button">
+                  <a href="#">
+                    Leer Más
+                    <i class="fa fa-chevron-right"></i>
+                  </a>
+                </div>
+                <div class="blog--social">
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-google-plus"></i></a>
+                </div>
+              </div>
+            </article>
+            <article class="blog--card">
+              <div class="blog--card__header">
+                <img src="views/assets/img/Recursos/images/img-blog-3.png" alt="">
+                <div class="blog--date">
+                  <h4 class="blog--date__title">
+                    13
+                  </h4>
+                  <h6 class="blog--date__subtitle">
+                    Junio
+                  </h6>
+                </div>
+                <div class="blog--card__title">
+                  <h6>130 Lecturas</h6>
+                  <h6>2 Comentarios</h6>
+                </div>
+              </div>
+              <div class="blog--card__body">
+                <div class="blog--card__pharagraf">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim.
+                </div>
+              </div>
+              <div class="blog--card__footer">
+                <div class="blog--button">
+                  <a href="#">
+                    Leer Más
+                    <i class="fa fa-chevron-right"></i>
+                  </a>
+                </div>
+                <div class="blog--social">
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-google-plus"></i></a>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+        <div class="events-container">
+          <div class="events--1">
+            <h6 class="events--1__subtitle">
+              Conoce los proximos
+            </h6>
+            <h2 class="events--1__title">
+              Eventos
+            </h2>
+          </div>
+          <div class="events--2">
+             <div class="events--2__section">
+               <h2 class="events--2__title">
+                 Maecenas Tristique Justo
+               </h2>
+               <h4 class="events--2__subtitle">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+               </h4>
+               <p class="events--2__text">
+                 Integer pulvinar justo sed ante egestas, sit amet molestie velit lacinia. Donec at nulla quis massa egestas tempor at eget est.
+               </p>
+             </div>
+          </div>
+        </div>
+        <section class="gallery-container">
+          <ul class="gallery--items">
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+            <li class="gallery-item">
+              <img src="" alt="">
+            </li>
+          </ul>
+        </section>
+        <section class="contact-container">
+          <div class="contact--header">
+            <img src="views/assets/img/Recursos/header/header-contact.png" alt="">
+            <h2 class="contact--title">Contáctame</h2>
+          </div>
+          <div class="contact--body">
+            <div class="form-container">
+              <form action="">
+                <div class="inputs-container-50">
+                  <label for="">Nombre Completo</label>
+                  <input type="text">
+                </div>
+                <div class="inputs-container-50">
+                  <label for="">Correo Electrónico</label>
+                  <input type="email">
+                </div>
+                <div class="inputs-container-100">
+                  <label for="">Deja tu Mensajes</label>
+                  <textarea name="" id=""></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="info-container">
+              <img src="views/assets/img/Recursos/logo/LogoGray.png" alt="">
+              <span class="info--title">Correo Electrónico</span>
+              <span class="info--subtitle">manager@julianosoriomusic</span>
+              <span class="info--title">Número Móvil</span>
+              <span class="info--subtitle">(57) 300 610 0192</span>
+            </div>
+          </div>
+          <footer class="contact-footer">
+            <div class="footer--copyright">
+              <h6 class="footer-text">
+                <i class="fa fa-copyright"></i>
+                2017 - 2018 Julian Osorio - Todos los derechos reservados.
+              </h6>
+            </div>
+            <div class="footer-logo">
+              <img src="views/assets/img/Recursos/logo/PoweredBy.png" alt="">
+            </div>
+          </footer>
+        </section> 
+      </main>
+      <script src="views/assets/js/jquery.min.js"></script>
+      <script src="views/assets/js/tether.min.js"></script>
+      <script src="views/assets/js/bootstrap.min.js"></script>
+      <script src="views/assets/js/index.js"></script>
+  </body>
+</html>
