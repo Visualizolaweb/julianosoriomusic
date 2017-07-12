37bfb04d27888917a62d6088aeba9a54a37e03b0
Daniel Cardona <decardona34@misena.edu.co>
2017-07-11 20:36:55 -0500

    Main Dashboard

--- dashboard/views/include/navbar.php
+++ dashboard/views/include/navbar.php
@@ -1,32 +1,31 @@
-<div class="navbar-container">
-    <nav class="navbar--contain">
-        <img class="image--icon icon--menu" src="views/assets/img/Recursos/icons/clear.svg" alt="">
-        <img class="image--icon icon--menu" src="views/assets/img/Recursos/icons/menu.svg" alt="">
-        <ul class="navbar--items">
-            <li class="navbar--item">
-                <img src="views/assets/img/Recursos/logo/Logo.png" alt="Logo Julián Osorio Music">
-            </li>
-            <li class="navbar--item">
-                <a href="#" class="navbar--link">Inicio</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#about-me" class="navbar--link">Banner</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#" class="navbar--link">Discografía</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#" class="navbar--link">Blog</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#" class="navbar--link">Eventos</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#" class="navbar--link">Galeria</a>
-            </li>
-            <li class="navbar--item">
-                <a href="#footer" class="navbar--link">Contáctame</a>
-            </li>
-        </ul>
-    </nav>
-</div>
+<nav class="navbar-container">
+    <ul class="navbar-items">
+        <li class="navbar-item">
+            <img class="navbar-image" src="views/assets/img/Recursos/logo/Logo.png" alt="Logo Julián Osorio Music">
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Inicio</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#about-me" class="navbar-link">Banner</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Discografía</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Blog</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Eventos</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Galeria</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Contáctame</a>
+        </li>
+        <li class="navbar-item">
+            <a href="#" class="navbar-link">Mi perfil</a>
+        </li>
+    </ul>
+</nav>
--- dashboard/views/modules/mod_user/user_manage/dashboard.php
+++ dashboard/views/modules/mod_user/user_manage/dashboard.php
@@ -1,33 +1,35 @@
 <div class="container--dashboard">
     <div class="container-fluid">
         <div class="row no-gutters">
-            <div class="col-12">
+            <header class="col-12 dashboard--menu">
                 <?php require_once 'views/include/navbar.php'; ?>
                 <div class="settings-container">
-                    <!-- <h3 class="settings--title">Daniel Cardona</h3> -->
-                    <i class="material-icons settings--icon">more_vert</i>
+                    <h3 class="settings-title">Daniel Cardona</h3>
                 </div>
-            </div>
+            </header>
             <div class="col-12">
                 <div class="banner--container">
-                    <div class="row no-gutters">
-                        <div class="col-12">
-                            <a href="#">Banner</a>
+                    <div class="row no-gutters banner-items">
+                        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 banner-item">
+                            <a class="banner-link" href="#">Banner</a>
                         </div>
-                        <div class="col-12">
-                            <a href="#">Discografía</a>
+                        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 banner-item">
+                            <a class="banner-link" href="#">Discografía</a>
                         </div>
-                        <div class="col-12">
-                            <a href="#">Blog</a>
+                        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 banner-item">
+                            <a class="banner-link" href="#">Blog</a>
                         </div>
-                        <div class="col-12">
-                            <a href="#">Eventos</a>
+                        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 banner-item">
+                            <a class="banner-link" href="#">Eventos</a>
                         </div>
-                        <div class="col-12">
-                            <a href="#">Galeria</a>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 banner-item">
+                            <a class="banner-link" href="#">Galeria</a>
                         </div>
-                        <div class="col-12">
-                            <a href="#">Contáctame</a>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 banner-item">
+                            <a class="banner-link" href="#">Contáctame</a>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 banner-item">
+                            <a class="banner-link" href="#">Mi perfil</a>
                         </div>
                     </div>
                 </div>
