5e3ad94e6752de943a16296428556b500da3faf1
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-07-17 23:13:18 -0500

    bd

--- model/db/julianosorio.sql
+++ model/db/julianosorio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-07-2017 a las 21:50:58
+-- Tiempo de generación: 18-07-2017 a las 06:12:55
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -58,7 +58,10 @@ CREATE TABLE `blog` (
 --
 
 INSERT INTO `blog` (`blo_id`, `blo_titulo`, `blo_descripcion`, `bli_id`) VALUES
-('LYI1vxUMVHeoP9tiU2mm35RE5qRnlt', 'Blog', 'Des Blog', 'bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR');
+('2PsEGeDj4cVI6O8oAaRbqH9T45RpN5', 'Bb', 'asd', 'FlmjCZPIp58vcQrUXBbL8RTI4oDiBM'),
+('KO791kETxJlKnLTaKXifb3JYEociIp', 'Hola', 'Que tal', 'KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1'),
+('LYI1vxUMVHeoP9tiU2mm35RE5qRnlt', 'Blog', 'Des Blog', 'bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR'),
+('rGILvhZS30IdTNKmdY9OfjBRBeBPFV', 'sad', 'asdasd', 'HxZhEDcplGBHaTh4vgAsUD8kXuGGi1');
 
 -- --------------------------------------------------------
 
@@ -78,7 +81,10 @@ CREATE TABLE `blog_imagen` (
 --
 
 INSERT INTO `blog_imagen` (`bli_id`, `bli_ruta`, `bli_fecha`, `bli_formulario`) VALUES
-('bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR', '1500147439.png', '2017-07-15 02:37:19', 'SI');
+('bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR', '1500147439.png', '2017-07-15 02:37:19', 'SI'),
+('FlmjCZPIp58vcQrUXBbL8RTI4oDiBM', '1500164268.png', '2017-07-15 07:17:48', 'SI'),
+('HxZhEDcplGBHaTh4vgAsUD8kXuGGi1', '1500169295.png', '2017-07-15 08:41:35', 'SI'),
+('KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1', '1500164163.png', '2017-07-15 07:16:03', 'SI');
 
 -- --------------------------------------------------------
 
@@ -97,7 +103,7 @@ CREATE TABLE `contacto` (
 --
 
 INSERT INTO `contacto` (`con_id`, `con_email`, `con_telefono`) VALUES
-('AeU839Cekf02Cw9fEcoe293c0vD2D1', 'manager@julianosoriomusic', '(57) 300 610 0192');
+('AeU839Cekf02Cw9fEcoe293c0vD2D1', 'manager@julianosoriomusic.com', '(57) 300 610 0192');
 
 -- --------------------------------------------------------
 
@@ -110,6 +116,33 @@ CREATE TABLE `discografia` (
   `dis_titulo` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
   `dis_cancion` varchar(120) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `evento_code` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `evento_nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `evento_descripcion` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `evento_direccion` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `evento_fecha_inicio` date NOT NULL,
+  `evento_hora_inicio` time NOT NULL,
+  `evento_fecha_fin` date NOT NULL,
+  `evento_hora_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`evento_code`, `evento_nombre`, `evento_descripcion`, `evento_direccion`, `evento_fecha_inicio`, `evento_hora_inicio`, `evento_fecha_fin`, `evento_hora_fin`) VALUES
+('02FUeFLGfJyjGC5', 'Mi pinche titulo', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Mi pinche direccion', '2017-07-16', '00:11:00', '1111-00-00', '11:00:00'),
+('8QGhdnpvGeodcLi', 'Mi pinche titulo2', 'Mi pinche descripcion222', 'Mi pinche direccion2222', '2017-07-18', '00:00:00', '0000-00-00', '00:00:00'),
+('x4ZzGDU6l9RUVgG', 'Pablo', 'hoa', 'pablito', '0000-11-11', '00:00:00', '0000-11-11', '00:00:00'),
+('YgjGChmNirUVaI3', 'title', 'descripcion', 'address', '2017-07-17', '00:00:00', '0000-00-00', '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -145,7 +178,35 @@ CREATE TABLE `galeria` (
 --
 
 INSERT INTO `galeria` (`gal_id`, `gal_ruta`) VALUES
-('h0CMd2Czp0tK9xkTOpi7hzH22GPIQz', '1500147587.png');
+('0tcMus5UMPvqRi9NlTgaNymOurfISO', '1500155198.png'),
+('9XKA8MFKgzOgY62hjrIJp6E7vumoe3', '1500177510.png'),
+('e8XEDum8dH692vLnQE4Sk8GbmeLImx', '1500177471.png'),
+('h0CMd2Czp0tK9xkTOpi7hzH22GPIQz', '1500147587.png'),
+('INrQBrNAUPK7AcSTgazG9tckcfZnTR', '1500177290.png'),
+('LYIkEA07OF9upL6fyaOAmYhkkKDFqX', '1500177450.png'),
+('uEFpJ7aXpP05opch4E9294JZQO6jhc', '1500177484.png'),
+('YVFvfFd9KE72tvbXDJstbqzSvVTyVl', '1500177497.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sobre`
+--
+
+CREATE TABLE `sobre` (
+  `sob_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `sob_frase` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `sob_parrafo1` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `sob_parrafo2` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `sob_ruta` varchar(120) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `sobre`
+--
+
+INSERT INTO `sobre` (`sob_id`, `sob_frase`, `sob_parrafo1`, `sob_parrafo2`, `sob_ruta`) VALUES
+('lO23cOv23PC2ei4dJV829CWVvivi32', 'mi vid', 'l', 'l', '1500257120.png');
 
 -- --------------------------------------------------------
 
@@ -203,6 +264,12 @@ ALTER TABLE `discografia`
   ADD PRIMARY KEY (`dis_id`);
 
 --
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`evento_code`);
+
+--
 -- Indices de la tabla `eventos_imagen`
 --
 ALTER TABLE `eventos_imagen`
@@ -213,6 +280,12 @@ ALTER TABLE `eventos_imagen`
 --
 ALTER TABLE `galeria`
   ADD PRIMARY KEY (`gal_id`);
+
+--
+-- Indices de la tabla `sobre`
+--
+ALTER TABLE `sobre`
+  ADD PRIMARY KEY (`sob_id`);
 
 --
 -- Indices de la tabla `usuario`
