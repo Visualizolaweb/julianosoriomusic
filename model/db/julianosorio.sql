10edec54061044f8491dbcad040a77dbb8e94d43
Pablo Restrepo <jprestrepo94@misena.edu.co>
2017-07-18 12:17:22 -0500

    bd

--- model/db/julianosorio.sql
+++ model/db/julianosorio.sql
@@ -3,6 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-07-2017 a las 19:16:23
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `julianosorio`
 --
-CREATE DATABASE IF NOT EXISTS `julianosorio` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `julianosorio`;
 
 -- --------------------------------------------------------
 
@@ -25,7 +26,6 @@ USE `julianosorio`;
 -- Estructura de tabla para la tabla `banner`
 --
 
-DROP TABLE IF EXISTS `banner`;
 CREATE TABLE `banner` (
   `ban_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `ban_subtitulo` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
@@ -46,23 +46,36 @@ INSERT INTO `banner` (`ban_id`, `ban_subtitulo`, `ban_titulo`, `ban_ruta`) VALUE
 -- Estructura de tabla para la tabla `blog`
 --
 
-DROP TABLE IF EXISTS `blog`;
 CREATE TABLE `blog` (
   `blo_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `blo_titulo` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
   `blo_descripcion` longtext COLLATE utf8_spanish_ci NOT NULL,
-  `bli_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+  `bli_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `blo_fecha` date NOT NULL,
+  `blo_lectura` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `blog`
 --
 
-INSERT INTO `blog` (`blo_id`, `blo_titulo`, `blo_descripcion`, `bli_id`) VALUES
-('2PsEGeDj4cVI6O8oAaRbqH9T45RpN5', 'Bb', 'asd', 'FlmjCZPIp58vcQrUXBbL8RTI4oDiBM'),
-('KO791kETxJlKnLTaKXifb3JYEociIp', 'Hola', 'Que tal', 'KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1'),
-('LYI1vxUMVHeoP9tiU2mm35RE5qRnlt', 'Blog', 'Des Blog', 'bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR'),
-('rGILvhZS30IdTNKmdY9OfjBRBeBPFV', 'sad', 'asdasd', 'HxZhEDcplGBHaTh4vgAsUD8kXuGGi1');
+INSERT INTO `blog` (`blo_id`, `blo_titulo`, `blo_descripcion`, `bli_id`, `blo_fecha`, `blo_lectura`) VALUES
+('2PsEGeDj4cVI6O8oAaRbqH9T45RpN5', 'Bb', 'asd', 'FlmjCZPIp58vcQrUXBbL8RTI4oDiBM', '2017-07-12', 0),
+('KO791kETxJlKnLTaKXifb3JYEociIp', 'Hola', 'Que tal', 'KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1', '2017-07-10', 0),
+('LYI1vxUMVHeoP9tiU2mm35RE5qRnlt', 'Blog', 'Des Blog', 'bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR', '2017-07-20', 0),
+('YXZsvcjoltHOlggjAqsQfiMZU24jNR', 'w', 'w', 'OU10zrKKitX9ipa0ZMrkLiCGgdu0dI', '2017-07-18', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `blog_comentario`
+--
+
+CREATE TABLE `blog_comentario` (
+  `com_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `blo_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `com_text` longtext COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -70,7 +83,6 @@ INSERT INTO `blog` (`blo_id`, `blo_titulo`, `blo_descripcion`, `bli_id`) VALUES
 -- Estructura de tabla para la tabla `blog_imagen`
 --
 
-DROP TABLE IF EXISTS `blog_imagen`;
 CREATE TABLE `blog_imagen` (
   `bli_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `bli_ruta` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
@@ -85,8 +97,8 @@ CREATE TABLE `blog_imagen` (
 INSERT INTO `blog_imagen` (`bli_id`, `bli_ruta`, `bli_fecha`, `bli_formulario`) VALUES
 ('bX3ZMlOXMTGAgtYPtjcl2CHBy8u3TR', '1500147439.png', '2017-07-15 02:37:19', 'SI'),
 ('FlmjCZPIp58vcQrUXBbL8RTI4oDiBM', '1500164268.png', '2017-07-15 07:17:48', 'SI'),
-('HxZhEDcplGBHaTh4vgAsUD8kXuGGi1', '1500169295.png', '2017-07-15 08:41:35', 'SI'),
-('KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1', '1500164163.png', '2017-07-15 07:16:03', 'SI');
+('KQ1FnOuFM5yhFJFBCRGnOJy2ZGoIx1', '1500164163.png', '2017-07-15 07:16:03', 'SI'),
+('OU10zrKKitX9ipa0ZMrkLiCGgdu0dI', '1500391118.png', '2017-07-18 10:18:38', 'SI');
 
 -- --------------------------------------------------------
 
@@ -94,7 +106,6 @@ INSERT INTO `blog_imagen` (`bli_id`, `bli_ruta`, `bli_fecha`, `bli_formulario`) 
 -- Estructura de tabla para la tabla `contacto`
 --
 
-DROP TABLE IF EXISTS `contacto`;
 CREATE TABLE `contacto` (
   `con_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `con_email` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
@@ -114,7 +125,6 @@ INSERT INTO `contacto` (`con_id`, `con_email`, `con_telefono`) VALUES
 -- Estructura de tabla para la tabla `discografia`
 --
 
-DROP TABLE IF EXISTS `discografia`;
 CREATE TABLE `discografia` (
   `dis_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `dis_titulo` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
@@ -126,7 +136,6 @@ CREATE TABLE `discografia` (
 --
 -- Estructura de tabla para la tabla `eventos`
 --
-
 
 CREATE TABLE `eventos` (
   `evento_code` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
@@ -146,6 +155,7 @@ CREATE TABLE `eventos` (
 INSERT INTO `eventos` (`evento_code`, `evento_nombre`, `evento_descripcion`, `evento_direccion`, `evento_fecha_inicio`, `evento_hora_inicio`, `evento_fecha_fin`, `evento_hora_fin`) VALUES
 ('02FUeFLGfJyjGC5', 'Mi pinche titulo', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Mi pinche direccion', '2017-07-16', '00:11:00', '1111-00-00', '11:00:00'),
 ('8QGhdnpvGeodcLi', 'Mi pinche titulo2', 'Mi pinche descripcion222', 'Mi pinche direccion2222', '2017-07-18', '00:00:00', '0000-00-00', '00:00:00'),
+('rUVaI3XkaLjhoN0', 'Marcha Gay', 'asomadre', 'Casa de MF', '2014-06-20', '10:20:00', '2014-06-20', '10:22:00'),
 ('x4ZzGDU6l9RUVgG', 'Pablo', 'hoa', 'pablito', '0000-11-11', '00:00:00', '0000-11-11', '00:00:00'),
 ('YgjGChmNirUVaI3', 'title', 'descripcion', 'address', '2017-07-17', '00:00:00', '0000-00-00', '00:00:00');
 
@@ -155,7 +165,6 @@ INSERT INTO `eventos` (`evento_code`, `evento_nombre`, `evento_descripcion`, `ev
 -- Estructura de tabla para la tabla `eventos_imagen`
 --
 
-DROP TABLE IF EXISTS `eventos_imagen`;
 CREATE TABLE `eventos_imagen` (
   `evi_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `evi_ruta` varchar(120) COLLATE utf8_spanish_ci DEFAULT NULL
@@ -166,7 +175,7 @@ CREATE TABLE `eventos_imagen` (
 --
 
 INSERT INTO `eventos_imagen` (`evi_id`, `evi_ruta`) VALUES
-('mek0YRcGTvUsfIGnhGDUf6ExBOecKV', '1500147468.png');
+('mek0YRcGTvUsfIGnhGDUf6ExBOecKV', '1500389910.png');
 
 -- --------------------------------------------------------
 
@@ -174,7 +183,6 @@ INSERT INTO `eventos_imagen` (`evi_id`, `evi_ruta`) VALUES
 -- Estructura de tabla para la tabla `galeria`
 --
 
-DROP TABLE IF EXISTS `galeria`;
 CREATE TABLE `galeria` (
   `gal_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `gal_ruta` varchar(70) COLLATE utf8_spanish_ci NOT NULL
@@ -194,6 +202,26 @@ INSERT INTO `galeria` (`gal_id`, `gal_ruta`) VALUES
 ('uEFpJ7aXpP05opch4E9294JZQO6jhc', '1500177484.png'),
 ('YVFvfFd9KE72tvbXDJstbqzSvVTyVl', '1500177497.png');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sobre`
+--
+
+CREATE TABLE `sobre` (
+  `sob_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `sob_frase` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `sob_parrafo1` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `sob_parrafo2` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `sob_ruta` varchar(120) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `sobre`
+--
+
+INSERT INTO `sobre` (`sob_id`, `sob_frase`, `sob_parrafo1`, `sob_parrafo2`, `sob_ruta`) VALUES
+('lO23cOv23PC2ei4dJV829CWVvivi32', 'mi vid', 'l', 'l', '1500389723.png');
 
 -- --------------------------------------------------------
 
@@ -201,7 +229,6 @@ INSERT INTO `galeria` (`gal_id`, `gal_ruta`) VALUES
 -- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `usu_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `usu_nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
@@ -232,6 +259,13 @@ ALTER TABLE `banner`
 ALTER TABLE `blog`
   ADD PRIMARY KEY (`blo_id`),
   ADD KEY `bli_id` (`bli_id`);
+
+--
+-- Indices de la tabla `blog_comentario`
+--
+ALTER TABLE `blog_comentario`
+  ADD PRIMARY KEY (`com_id`),
+  ADD KEY `blo_id` (`blo_id`);
 
 --
 -- Indices de la tabla `blog_imagen`
@@ -290,6 +324,12 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `blog`
   ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`bli_id`) REFERENCES `blog_imagen` (`bli_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `blog_comentario`
+--
+ALTER TABLE `blog_comentario`
+  ADD CONSTRAINT `blog_comentario_ibfk_1` FOREIGN KEY (`blo_id`) REFERENCES `blog` (`blo_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
