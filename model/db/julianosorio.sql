5e0439f5036fae62983b05ffd51f1d1ba505a87e
Daniel Cardona <decardona34@misena.edu.co>
2017-08-17 15:45:08 -0500

    BD

--- model/db/julianosorio.sql
+++ model/db/julianosorio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-08-2017 a las 23:31:57
+-- Tiempo de generación: 17-08-2017 a las 22:44:16
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 5.6.31
 
@@ -40,7 +40,7 @@ CREATE TABLE `banner` (
 --
 
 INSERT INTO `banner` (`ban_id`, `ban_subtitulo`, `ban_titulo`, `ban_ruta`) VALUES
-('2VXe2DUcj3I4KnfeF33CFCfrWREs32', 'Descarga la aplicacións', 'Infinitos', '1502909425.png');
+('2VXe2DUcj3I4KnfeF33CFCfrWREs32', 'Descarga la aplicacións', 'Infinitos', '1502985213.png');
 
 -- --------------------------------------------------------
 
@@ -62,8 +62,7 @@ CREATE TABLE `blog` (
 --
 
 INSERT INTO `blog` (`blo_id`, `blo_titulo`, `blo_descripcion`, `bli_id`, `blo_fecha`, `blo_lectura`) VALUES
-('jEZI227oqQbmk4otAclQxx57RZq9TP', 'Peloooooooos', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim', '6hH8n7LovBVbjuj0rcS5a3iSbkvD2O', '2017-07-19', 2),
-('Kf5kUtm2CgsjXEDX5F2yyzU8k2cCYu', 'Holaaa', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim', 'ogLy9nGYZA6zPOTd6ZiLRGUQ7b8mp1', '2017-07-19', 1);
+('Kf5kUtm2CgsjXEDX5F2yyzU8k2cCYu', 'Holaaa', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae, maiores, magni dolorum aliquam culpa inventore ullam quae qui optio obcaecati doloribus minus perferendis error dolor blanditiis! Culpa, enim', 'ogLy9nGYZA6zPOTd6ZiLRGUQ7b8mp1', '2017-07-19', 3);
 
 -- --------------------------------------------------------
 
@@ -82,7 +81,7 @@ CREATE TABLE `blog_comentario` (
 --
 
 INSERT INTO `blog_comentario` (`com_id`, `blo_id`, `com_text`) VALUES
-('74345', 'jEZI227oqQbmk4otAclQxx57RZq9TP', 'adasfdas');
+('eo8uPQhie4ZbBkE', 'Kf5kUtm2CgsjXEDX5F2yyzU8k2cCYu', 'Hola');
 
 -- --------------------------------------------------------
 
@@ -102,7 +101,6 @@ CREATE TABLE `blog_imagen` (
 --
 
 INSERT INTO `blog_imagen` (`bli_id`, `bli_ruta`, `bli_fecha`, `bli_formulario`) VALUES
-('6hH8n7LovBVbjuj0rcS5a3iSbkvD2O', '1500516016.png', '2017-07-19 09:00:16', 'SI'),
 ('ogLy9nGYZA6zPOTd6ZiLRGUQ7b8mp1', '1500515402.png', '2017-07-19 08:50:02', 'SI');
 
 -- --------------------------------------------------------
@@ -133,8 +131,20 @@ INSERT INTO `contacto` (`con_id`, `con_email`, `con_telefono`) VALUES
 CREATE TABLE `discografia` (
   `dis_id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `dis_titulo` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
-  `dis_cancion` varchar(120) COLLATE utf8_spanish_ci NOT NULL
+  `dis_cancion` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  `dis_letra` longtext COLLATE utf8_spanish_ci NOT NULL,
+  `dis_estado` char(20) COLLATE utf8_spanish_ci NOT NULL,
+  `dis_num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `discografia`
+--
+
+INSERT INTO `discografia` (`dis_id`, `dis_titulo`, `dis_cancion`, `dis_letra`, `dis_estado`, `dis_num`) VALUES
+('bRg0UvBinT1cTgH80enoKyQJviNMmV', 'Kalimba', 'Kalimba.mp3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet risus non velit auctor sodales. Donec sapien ligula, condimentum sed malesuada non, varius at enim. In ligula ipsum, sodales non nisi quis, placerat ultricies arcu. Duis tristique odio at lectus imperdiet, id aliquet nisi condimentum. Nunc id turpis aliquet turpis lobortis faucibus ut ut risus. Maecenas fermentum elit vitae consequat porta. Maecenas mattis suscipit dapibus. Ut consequat pharetra erat, a malesuada lorem tincidunt eu. Morbi rhoncus auctor interdum. Nam ullamcorper tincidunt mattis. Praesent facilisis tortor nisi, pharetra mollis ante accumsan posuere. Nam volutpat justo quis nibh porta, et euismod mauris hendrerit.\r\n\r\nDuis et feugiat sapien. Cras in finibus sem. Pellentesque a dolor mauris. Vivamus id iaculis metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nisl velit, dapibus eget consectetur sit amet, bibendum sed dui. Sed luctus accumsan sem, non aliquet risus. Sed nulla sem, lacinia consequat est et, rutrum maximus nibh. Praesent faucibus neque et est fermentum pharetra. Etiam laoreet lectus eu orci euismod sagittis. Nullam fermentum et risus eget gravida. In non consequat augue, eu sodales mauris.\r\n\r\nPellentesque quis lorem non ante pretium fermentum. Maecenas semper ornare risus sed convallis. Vivamus orci sem, maximus ac turpis blandit, ultrices scelerisque arcu. Quisque eu urna eu dui mattis semper. Nulla facilisi. Mauris luctus, neque non tristique dignissim, neque tellus tincidunt purus, sed vestibulum augue neque nec ante. Sed in posuere libero.\r\n\r\nProin posuere risus sit amet suscipit pretium. Integer orci magna, iaculis in euismod eu, venenatis a urna. Etiam porttitor placerat sem, in consectetur nulla lacinia ac. Pellentesque non vulputate ligula, vel malesuada diam. Phasellus eu convallis mauris. Etiam cursus turpis justo, ut euismod ligula efficitur non. Integer a consectetur lorem. Maecenas in mi lorem. Nullam hendrerit luctus dui eu tempus.\r\n\r\nPhasellus ac diam et lacus pulvinar iaculis sit amet non lorem. Duis nulla neque, aliquet a dictum quis, dapibus pharetra arcu. Maecenas placerat condimentum interdum. Maecenas fringilla, nibh efficitur mattis volutpat, nisi nisi euismod neque, eget scelerisque magna nisl ut sem. Morbi consectetur nulla ligula. Sed accumsan convallis bibendum. Praesent faucibus, augue sit amet consectetur feugiat, nunc est maximus eros, in semper arcu tellus viverra urna. Morbi laoreet fermentum interdum. Cras quis nisi sit amet neque ornare suscipit sed quis nibh.', 'Activo', 1),
+('lcL9Fsq5PQnS1Dmumjtc1vjIcHNB0k', 'Canción para mentir', 'Maid with the Flaxen Hair.mp3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet risus non velit auctor sodales. Donec sapien ligula, condimentum sed malesuada non, varius at enim. In ligula ipsum, sodales non nisi quis, placerat ultricies arcu. Duis tristique odio at lectus imperdiet, id aliquet nisi condimentum. Nunc id turpis aliquet turpis lobortis faucibus ut ut risus. Maecenas fermentum elit vitae consequat porta. Maecenas mattis suscipit dapibus. Ut consequat pharetra erat, a malesuada lorem tincidunt eu. Morbi rhoncus auctor interdum. Nam ullamcorper tincidunt mattis. Praesent facilisis tortor nisi, pharetra mollis ante accumsan posuere. Nam volutpat justo quis nibh porta, et euismod mauris hendrerit.\r\n\r\nDuis et feugiat sapien. Cras in finibus sem. Pellentesque a dolor mauris. Vivamus id iaculis metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nisl velit, dapibus eget consectetur sit amet, bibendum sed dui. Sed luctus accumsan sem, non aliquet risus. Sed nulla sem, lacinia consequat est et, rutrum maximus nibh. Praesent faucibus neque et est fermentum pharetra. Etiam laoreet lectus eu orci euismod sagittis. Nullam fermentum et risus eget gravida. In non consequat augue, eu sodales mauris.\r\n\r\nPellentesque quis lorem non ante pretium fermentum. Maecenas semper ornare risus sed convallis. Vivamus orci sem, maximus ac turpis blandit, ultrices scelerisque arcu. Quisque eu urna eu dui mattis semper. Nulla facilisi. Mauris luctus, neque non tristique dignissim, neque tellus tincidunt purus, sed vestibulum augue neque nec ante. Sed in posuere libero.\r\n\r\nProin posuere risus sit amet suscipit pretium. Integer orci magna, iaculis in euismod eu, venenatis a urna. Etiam porttitor placerat sem, in consectetur nulla lacinia ac. Pellentesque non vulputate ligula, vel malesuada diam. Phasellus eu convallis mauris. Etiam cursus turpis justo, ut euismod ligula efficitur non. Integer a consectetur lorem. Maecenas in mi lorem. Nullam hendrerit luctus dui eu tempus.\r\n\r\nPhasellus ac diam et lacus pulvinar iaculis sit amet non lorem. Duis nulla neque, aliquet a dictum quis, dapibus pharetra arcu. Maecenas placerat condimentum interdum. Maecenas fringilla, nibh efficitur mattis volutpat, nisi nisi euismod neque, eget scelerisque magna nisl ut sem. Morbi consectetur nulla ligula. Sed accumsan convallis bibendum. Praesent faucibus, augue sit amet consectetur feugiat, nunc est maximus eros, in semper arcu tellus viverra urna. Morbi laoreet fermentum interdum. Cras quis nisi sit amet neque ornare suscipit sed quis nibh.\r\n\r\nUt nec est et mi dapibus iaculis et ac libero. Proin fermentum congue varius. Aliquam auctor cursus mauris at vehicula. Ut aliquet turpis nunc, vitae pulvinar mi condimentum in. Nam nec congue mi, ullamcorper lobortis est. Nullam aliquam augue accumsan mi aliquet, rhoncus sagittis lorem bibendum. Curabitur dapibus, nisl non suscipit elementum, elit lorem aliquet velit, aliquet vestibulum justo ante quis sapien. In scelerisque justo et ornare tristique. Nulla enim nulla, euismod quis bibendum eu, semper eget justo.\r\n\r\nAliquam non neque at nisi fermentum luctus nec gravida nisi. Donec a dui vel massa gravida rutrum at et enim. Integer lobortis urna nec vulputate blandit. Proin id nibh aliquam, molestie magna quis, ultricies orci. Maecenas sed eleifend dui, nec elementum odio. Nam et mi consequat urna laoreet sollicitudin. Quisque sit amet dolor vitae erat commodo suscipit eu non est. In aliquam euismod nisi. Integer nibh nibh, malesuada in dui id, pulvinar congue sem. Mauris vestibulum eros quis ipsum vestibulum volutpat. Vivamus lobortis enim mi, dapibus facilisis sem ullamcorper id.\r\n\r\nMauris convallis finibus erat, in fringilla urna venenatis in. Nunc volutpat orci risus, at finibus dolor tristique pretium. Phasellus efficitur lorem sit amet euismod semper. Proin in mauris eget odio tempus interdum. Nunc auctor vehicula massa eget posuere. Vestibulum ornare augue quis mauris hendrerit fringilla. Nam eu massa nunc. Curabitur vulputate ligula sed risus tempor, eget vestibulum leo luctus. Nunc dui nulla, efficitur et condimentum condimentum, laoreet ut massa. Donec malesuada ullamcorper scelerisque. Nullam ullamcorper orci augue, ac convallis dolor commodo elementum. Sed hendrerit sed nisl eget consectetur. Nunc posuere nisl lectus, sit amet porta nulla pharetra at. Sed vitae felis lacinia, porttitor diam sed, ultrices dui. Nullam rutrum pulvinar facilisis. Quisque id viverra nunc.', 'Activo', 3),
+('NIMRezc4sk8ZKRZRhPhpCUNJZ8400s', 'Sleep Away', 'Sleep Away.mp3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet risus non velit auctor sodales. Donec sapien ligula, condimentum sed malesuada non, varius at enim. In ligula ipsum, sodales non nisi quis, placerat ultricies arcu. Duis tristique odio at lectus imperdiet, id aliquet nisi condimentum. Nunc id turpis aliquet turpis lobortis faucibus ut ut risus. Maecenas fermentum elit vitae consequat porta. Maecenas mattis suscipit dapibus. Ut consequat pharetra erat, a malesuada lorem tincidunt eu. Morbi rhoncus auctor interdum. Nam ullamcorper tincidunt mattis. Praesent facilisis tortor nisi, pharetra mollis ante accumsan posuere. Nam volutpat justo quis nibh porta, et euismod mauris hendrerit.\r\n\r\nDuis et feugiat sapien. Cras in finibus sem. Pellentesque a dolor mauris. Vivamus id iaculis metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nisl velit, dapibus eget consectetur sit amet, bibendum sed dui. Sed luctus accumsan sem, non aliquet risus. Sed nulla sem, lacinia consequat est et, rutrum maximus nibh. Praesent faucibus neque et est fermentum pharetra. Etiam laoreet lectus eu orci euismod sagittis. Nullam fermentum et risus eget gravida. In non consequat augue, eu sodales mauris.\r\n\r\nPellentesque quis lorem non ante pretium fermentum. Maecenas semper ornare risus sed convallis. Vivamus orci sem, maximus ac turpis blandit, ultrices scelerisque arcu. Quisque eu urna eu dui mattis semper. Nulla facilisi. Mauris luctus, neque non tristique dignissim, neque tellus tincidunt purus, sed vestibulum augue neque nec ante. Sed in posuere libero.\r\n\r\nProin posuere risus sit amet suscipit pretium. Integer orci magna, iaculis in euismod eu, venenatis a urna. Etiam porttitor placerat sem, in consectetur nulla lacinia ac. Pellentesque non vulputate ligula, vel malesuada diam. Phasellus eu convallis mauris. Etiam cursus turpis justo, ut euismod ligula efficitur non. Integer a consectetur lorem. Maecenas in mi lorem. Nullam hendrerit luctus dui eu tempus.\r\n\r\nPhasellus ac diam et lacus pulvinar iaculis sit amet non lorem. Duis nulla neque, aliquet a dictum quis, dapibus pharetra arcu. Maecenas placerat condimentum interdum. Maecenas fringilla, nibh efficitur mattis volutpat, nisi nisi euismod neque, eget scelerisque magna nisl ut sem. Morbi consectetur nulla ligula. Sed accumsan convallis bibendum. Praesent faucibus, augue sit amet consectetur feugiat, nunc est maximus eros, in semper arcu tellus viverra urna. Morbi laoreet fermentum interdum. Cras quis nisi sit amet neque ornare suscipit sed quis nibh.', 'Activo', 2);
 
 -- --------------------------------------------------------
 
@@ -161,7 +171,6 @@ INSERT INTO `eventos` (`evento_code`, `evento_nombre`, `evento_descripcion`, `ev
 ('02FUeFLGfJyjGC5', 'Mi pinche titulo', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Mi pinche direccion', '2017-07-19', '00:11:00', '1111-00-00', '11:00:00'),
 ('8QGhdnpvGeodcLi', 'Mi pinche titulo2', 'Mi pinche descripcion222', 'Mi pinche direccion2222', '2017-07-18', '00:00:00', '0000-00-00', '00:00:00'),
 ('rUVaI3XkaLjhoN0', 'Marcha Gay', 'asomadre', 'Casa de MF', '2018-06-20', '10:20:00', '2018-06-20', '10:22:00'),
-('x4ZzGDU6l9RUVgG', 'Pablo', 'hoa', 'pablito', '0000-11-11', '00:00:00', '0000-11-11', '00:00:00'),
 ('YgjGChmNirUVaI3', 'title', 'descripcion', 'address', '2017-07-23', '00:00:00', '0000-00-00', '00:00:00');
 
 -- --------------------------------------------------------
@@ -180,7 +189,7 @@ CREATE TABLE `eventos_imagen` (
 --
 
 INSERT INTO `eventos_imagen` (`evi_id`, `evi_ruta`) VALUES
-('mek0YRcGTvUsfIGnhGDUf6ExBOecKV', '1502910010.png');
+('mek0YRcGTvUsfIGnhGDUf6ExBOecKV', '1502986049.png');
 
 -- --------------------------------------------------------
 
@@ -226,7 +235,7 @@ CREATE TABLE `sobre` (
 --
 
 INSERT INTO `sobre` (`sob_id`, `sob_frase`, `sob_parrafo1`, `sob_parrafo2`, `sob_ruta`) VALUES
-('lO23cOv23PC2ei4dJV829CWVvivi32', 'Phasellus pablo ullamcorper magna id sagittis. Finibus ante non iaculis. purus quis bibendum elementum Ut lorem tortor', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor purus quis bibendum elementum. Nulla luctus, velit vel dignissim volutpat, elit neque ullamcorper eros, vel commodo enim ex vel erat. Etiam nisi odio, dictum in scelerisque ut, accumsan in tortor. Etiam consequat nibh sit amet varius posuere. Integer pulvinar justo sed ante egestas, sit amet molestie velit lacinia.', 'Donec at nulla quis massa egestas tempor at eget est. Maecenas eget laoreet ante, ut egestas turpis. Maecenas sed elit vel arcu sagittis suscipit. Aliquam erat volutpat. Sed sit amet arcu et elit congue bibendum eget in ante. Nunc ligula augue, aliquam et nulla non, bibendum imperdiet diam. Phasellus semper, nisi at fringilla rhoncus, mauris diam dapibus purus, eu varius odio mi vitae mauris. Aenean pellentesque diam luctus augue dapibus luctus.\n', '1502907300.png');
+('lO23cOv23PC2ei4dJV829CWVvivi32', 'Phasellus pablo ullamcorper magna id sagittis. Finibus ante non iaculis. purus quis bibendum elementum Ut lorem tortor', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor purus quis bibendum elementum. Nulla luctus, velit vel dignissim volutpat, elit neque ullamcorper eros, vel commodo enim ex vel erat. Etiam nisi odio, dictum in scelerisque ut, accumsan in tortor. Etiam consequat nibh sit amet varius posuere. Integer pulvinar justo sed ante egestas, sit amet molestie velit lacinia.', 'Donec at nulla quis massa egestas tempor at eget est. Maecenas eget laoreet ante, ut egestas turpis. Maecenas sed elit vel arcu sagittis suscipit. Aliquam erat volutpat. Sed sit amet arcu et elit congue bibendum eget in ante. Nunc ligula augue, aliquam et nulla non, bibendum imperdiet diam. Phasellus semper, nisi at fringilla rhoncus, mauris diam dapibus purus, eu varius odio mi vitae mauris. Aenean pellentesque diam luctus augue dapibus luctus.\n', '1502986290.png');
 
 -- --------------------------------------------------------
 
@@ -246,7 +255,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`usu_id`, `usu_nombre`, `usu_email`, `usu_password`) VALUES
-('AXiZqbBLuCd7y9PA6IMeqjqzMZh2FV', 'Julian Osorio', 'manager@julianosoriomusic', '$2y$10$BUc.LIx5BMNozchNG/KgH.Uv4oeMLAn4bk7V4llQ6SzMmfDib1j4m');
+('AXiZqbBLuCd7y9PA6IMeqjqzMZh2FV', 'Julian Osorio', 'manager@julianosoriomusic', '$2y$10$LvgBl0H/Qsd5ncGDhl3ha.1c2NkeyP70uuSEfYKz19.4U7qcY/twe');
 
 --
 -- Índices para tablas volcadas
