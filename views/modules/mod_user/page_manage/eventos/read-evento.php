cd3f9ae6d5c80f95b48b8daabee1802cc5929982
Daniel Cardona <decardona34@misena.edu.co>
2017-08-26 14:05:17 -0500

    Estilos read evento

--- views/modules/mod_user/page_manage/eventos/read-evento.php
+++ views/modules/mod_user/page_manage/eventos/read-evento.php
@@ -40,14 +40,78 @@
           </nav>
       </div>
     </div>
-  </header>
+    <?php
+    function convertMonthEvent($month){
+        switch ($month) {
+            case '01':
+                $month = "Enero";
+                break;
+            case '02':
+                $month = "Febrero";
+                break;
+            case '03':
+                $month = "Marzo";
+                break;
+            case '04':
+                $month = "Abril";
+                break;
+            case '05':
+                $month = "Mayo";
+                break;
+            case '06':
+                $month = "Junio";
+                break;
+            case '07':
+                $month = "Julio";
+                break;
+            case '08':
+                $month = "Agosto";
+                break;
+            case '09':
+                $month = "Septiembre";
+                break;
+            case '10':
+                $month = "Octubre";
+                break;
+            case '11':
+                $month = "Noviembre";
+                break;
+            case '12':
+                $month = "Diciembre";
+                break;
+            default:
+                $month = "Enero";
+                break;
+
+        }
+        return $month;
+    }
+    list($years, $months, $days) = explode("-", $event["evento_fecha_inicio"]);
+    list($yearss, $monthss, $dayss) = explode("-", $event["evento_fecha_fin"]);
+    ?>
+    <section class="blog--container__one blog--container__main blog--body__full">
+    <article class="blog--content event--content">
+        <h1 class="blog--content__title"><?php echo $event["evento_nombre"]; ?></h1>
+        <h3 class="blog--content__subtitle"><?php echo $event["evento_direccion"]; ?></h3>
+        <h5 class="blog--datetime">
+            <?php echo "Comienza el $days de ".convertMonthEvent($months)." del $years".' a las '.substr($event["evento_hora_inicio"],3); ?>
+        </h5>
+        <h5 class="blog--datetime">
+            <?php echo "Termina el $dayss de ".convertMonthEvent($monthss)." del $yearss".' a las '.substr($event["evento_hora_fin"],3); ?>
+        </h5>
+        <p class="blog--content__text"><?php echo $event["evento_descripcion"]; ?></p>
+    </article>
+    <div class="recent--blogs">
+      <h5 class="recent--text">Próximos eventos</h5>
+      <?php
+      foreach ($this->PaginaM->readUpcomingEventsAndroid() as $row) {
+
+      ?>
+      <a class="recent--link" href="<?php echo "eventos&token=".$row['evento_code'];?>"><?php echo $row["evento_nombre"]; ?></a>
+      <?php
+      }
+      ?>
+    </div>
+  </section>
+</header>
 </main>
-<div class="">
-    <h1><?php echo $event["evento_nombre"]; ?></h1>
-    <h2><?php echo $event["evento_direccion"]; ?></h2>
-    <p><?php echo $event["evento_descripcion"]; ?></p>
-    <h4><?php echo $event["evento_fecha_inicio"]; ?></h4>
-    <h4><?php echo substr($event["evento_hora_inicio"],3); ?></h4>
-    <h4><?php echo $event["evento_fecha_fin"]; ?></h4>
-    <h4><?php echo substr($event["evento_hora_fin"],3); ?></h4>
-</div>
